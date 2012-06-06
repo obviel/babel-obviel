54671a81c527d526704a57eae67262c22600d31d
faassen <faassen@startifact.com>
2012-06-06 17:39:16 +0200

    Back to development: 0.7

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (
 
 setup(
     name='babel-obviel',
-    version='0.6',
+    version='0.7.dev0',
     description='Obviel Template message extractor for Babel',
     long_description=long_description,
     author='Obviel Developers',
