fd817c1c59b7958a9d49aae1df6f46745d1b9c22
faassen <faassen@startifact.com>
2012-06-06 17:08:46 +0200

    Preparing release 0.5.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (
 
 setup(
     name='babel-obviel',
-    version='0.6.dev0',
+    version='0.5.1',
     description='Obviel Template message extractor for Babel',
     long_description=long_description,
     author='Obviel Developers',
